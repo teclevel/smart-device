31e085355bc26b7dfa58d6497ceabf4d88da2892
Sergey <szaytsev060576@gmail.com>
2021-06-15 23:21:48 +0300

    подготавливает проект

---
+++ source/js/script.js
@@ -0,0 +1,29 @@
+// 'use strict';
+// var pageHeader = document.querySelector('.header');
+// var headerToggle = document.querySelector('.header__toggle-menu');
+// var pageBody = document.querySelector('body');
+// var linkItemMenu = pageHeader.querySelectorAll('a');
+
+
+// if (pageHeader) {
+//   pageHeader.classList.remove('header--no-js');
+// }
+
+
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('header--open-menu')) {
+//     pageHeader.classList.remove('header--open-menu');
+//     pageBody.style.overflowY = 'scroll';
+//   } else {
+//     pageHeader.classList.add('header--open-menu');
+//     pageBody.style.overflowY = 'hidden';
+//   }
+// });
+
+
+// linkItemMenu.forEach(function (item) {
+//   item.addEventListener('click', function () {
+//     pageBody.style.overflowY = 'scroll';
+//     pageHeader.classList.remove('header--open-menu');
+//   });
+// });
