d2f7f22893900813a484b7bcbe686e5b9bf82fa6
Sergey <szaytsev060576@gmail.com>
2021-06-20 17:25:04 +0300

    стилизует блок about

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,4 @@
-// // 'use strict';
+// 'use strict';
 // const pageHeader = document.querySelector('.header');
 // const headerToggle = document.querySelector('.header__toggle-menu');
 // const pageBody = document.querySelector('body');
@@ -27,3 +27,19 @@
 //     pageHeader.classList.remove('header--open-menu');
 //   });
 // });
+const cropText = function() {
+  const cropElement = document.querySelectorAll('.about__text-crop');
+  const size = 200;
+  const endCharacter = '..';
+
+  cropElement.forEach((el) => {
+    let text = el.innerHTML;
+
+    if (el.innerHTML.length > size) {
+      text = text.substr(0, size);
+      el.innerHTML = text + endCharacter;
+    }
+  });
+};
+
+// cropText();
