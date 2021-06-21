8038dc3a3f85ede86319b28410fc8cb7069241a3
Sergey <szaytsev060576@gmail.com>
2021-06-21 16:27:38 +0300

    стилизует блок offer

--- build/js/script.js
+++ build/js/script.js
@@ -1,61 +1,61 @@
-// 'use strict';
-// const pageHeader = document.querySelector('.header');
-// const headerToggle = document.querySelector('.header__toggle-menu');
-// const pageBody = document.querySelector('body');
-// const linkItemMenu = pageHeader.querySelectorAll('a');
+// // 'use strict';
+// // const pageHeader = document.querySelector('.header');
+// // const headerToggle = document.querySelector('.header__toggle-menu');
+// // const pageBody = document.querySelector('body');
+// // const linkItemMenu = pageHeader.querySelectorAll('a');
 
 
-// if (pageHeader) {
-//   pageHeader.classList.remove('header--no-js');
-// }
+// // if (pageHeader) {
+// //   pageHeader.classList.remove('header--no-js');
+// // }
 
 
-// headerToggle.addEventListener('click', () => {
-//   if (pageHeader.classList.contains('header--open-menu')) {
-//     pageHeader.classList.remove('header--open-menu');
-//     pageBody.style.overflowY = 'scroll';
+// // headerToggle.addEventListener('click', () => {
+// //   if (pageHeader.classList.contains('header--open-menu')) {
+// //     pageHeader.classList.remove('header--open-menu');
+// //     pageBody.style.overflowY = 'scroll';
+// //   } else {
+// //     pageHeader.classList.add('header--open-menu');
+// //     pageBody.style.overflowY = 'hidden';
+// //   }
+// // });
+
+
+// // linkItemMenu.forEach((item) => {
+// //   item.addEventListener('click',() => {
+// //     pageBody.style.overflowY = 'scroll';
+// //     pageHeader.classList.remove('header--open-menu');
+// //   });
+// // });
+
+// /* Обрезка текста*/
+
+// const textAbout = document.querySelector('.about__text-crop');
+// const content = textAbout.textContent;
+// const buttonCropText = document.querySelector('.about__button');
+
+// const cropText = function() {
+//   const cropElements = document.querySelectorAll('.about__text-crop');
+//   const size = 200;
+//   const endCharacter = '..';
+
+//   cropElements.forEach((el) => {
+//     let text = el.innerHTML;
+//     if (el.innerHTML.length > size) {
+//       text = text.substring(0, size);
+//       el.innerHTML = text + endCharacter;
+//     }
+//   });
+// };
+
+// cropText();
+
+// buttonCropText.addEventListener('click', () => {
+//   if (buttonCropText.classList.contains('text-cropped')) {
+//     buttonCropText.classList.remove('text-cropped');
+//     textAbout.textContent = content;
 //   } else {
-//     pageHeader.classList.add('header--open-menu');
-//     pageBody.style.overflowY = 'hidden';
+//     cropText();
+//     buttonCropText.classList.add('text-cropped');
 //   }
 // });
-
-
-// linkItemMenu.forEach((item) => {
-//   item.addEventListener('click',() => {
-//     pageBody.style.overflowY = 'scroll';
-//     pageHeader.classList.remove('header--open-menu');
-//   });
-// });
-
-/* Обрезка текста*/
-
-const textAbout = document.querySelector('.about__text-crop');
-const content = textAbout.textContent;
-const buttonCropText = document.querySelector('.about__button');
-
-const cropText = function() {
-  const cropElements = document.querySelectorAll('.about__text-crop');
-  const size = 200;
-  const endCharacter = '..';
-
-  cropElements.forEach((el) => {
-    let text = el.innerHTML;
-    if (el.innerHTML.length > size) {
-      text = text.substring(0, size);
-      el.innerHTML = text + endCharacter;
-    }
-  });
-};
-
-cropText();
-
-buttonCropText.addEventListener('click', () => {
-  if (buttonCropText.classList.contains('text-cropped')) {
-    buttonCropText.classList.remove('text-cropped');
-    textAbout.textContent = content;
-  } else {
-    cropText();
-    buttonCropText.classList.add('text-cropped');
-  }
-});
